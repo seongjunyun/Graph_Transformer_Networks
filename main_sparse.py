2224a7c3e2516d96bda791e1f9c14b9bbbc3c6e4
seongjunyun <ysj5419@korea.ac.kr>
2019-11-25 14:35:38 +0900

    Add files via upload

---
+++ main_sparse.py
@@ -0,0 +1,146 @@
+import torch
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+from model_sparse import GTN
+from matplotlib import pyplot as plt
+import pdb
+from torch_geometric.utils import dense_to_sparse, f1_score, accuracy
+from torch_geometric.data import Data
+import torch_sparse
+import pickle
+#from mem import mem_report
+from scipy.sparse import csr_matrix
+import scipy.sparse as sp
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', type=str,
+                        help='Dataset')
+    parser.add_argument('--epoch', type=int, default=40,
+                        help='Training Epochs')
+    parser.add_argument('--node_dim', type=int, default=64,
+                        help='Node dimension')
+    parser.add_argument('--num_channels', type=int, default=2,
+                        help='number of channels')
+    parser.add_argument('--lr', type=float, default=0.005,
+                        help='learning rate')
+    parser.add_argument('--weight_decay', type=float, default=0.001,
+                        help='l2 reg')
+    parser.add_argument('--num_layers', type=int, default=3,
+                        help='number of layer')
+    parser.add_argument('--norm', type=str, default='true',
+                        help='normalization')
+    parser.add_argument('--adaptive_lr', type=str, default='false',
+                    help='adaptive learning rate')
+
+    args = parser.parse_args()
+    print(args)
+    epochs = args.epoch
+    node_dim = args.node_dim
+    num_channels = args.num_channels
+    lr = args.lr
+    weight_decay = args.weight_decay
+    num_layers = args.num_layers
+    norm = args.norm
+    adaptive_lr = args.adaptive_lr
+
+    with open('data/'+args.dataset+'/node_features.pkl','rb') as f:
+        node_features = pickle.load(f)
+    with open('data/'+args.dataset+'/edges.pkl','rb') as f:
+        edges = pickle.load(f)
+    with open('data/'+args.dataset+'/labels.pkl','rb') as f:
+        labels = pickle.load(f)
+        
+        
+    num_nodes = edges[0].shape[0]
+    A = []
+    
+    for i,edge in enumerate(edges):
+        edge_tmp = torch.from_numpy(np.vstack((edge.nonzero()[0], edge.nonzero()[1]))).type(torch.cuda.LongTensor)
+        value_tmp = torch.ones(edge_tmp.shape[1]).type(torch.cuda.FloatTensor)
+        A.append((edge_tmp,value_tmp))
+    edge_tmp = torch.stack((torch.arange(0,num_nodes),torch.arange(0,num_nodes))).type(torch.cuda.LongTensor)
+    value_tmp = torch.ones(num_nodes).type(torch.cuda.FloatTensor)
+    A.append((edge_tmp,value_tmp))
+
+    node_features = torch.from_numpy(node_features).type(torch.cuda.FloatTensor)
+    train_node = torch.from_numpy(np.array(labels[0])[:,0]).type(torch.cuda.LongTensor)
+    train_target = torch.from_numpy(np.array(labels[0])[:,1]).type(torch.cuda.LongTensor)
+
+    valid_node = torch.from_numpy(np.array(labels[1])[:,0]).type(torch.cuda.LongTensor)
+    valid_target = torch.from_numpy(np.array(labels[1])[:,1]).type(torch.cuda.LongTensor)
+    test_node = torch.from_numpy(np.array(labels[2])[:,0]).type(torch.cuda.LongTensor)
+    test_target = torch.from_numpy(np.array(labels[2])[:,1]).type(torch.cuda.LongTensor)
+
+
+    num_classes = torch.max(train_target).item()+1
+
+    train_losses = []
+    train_f1s = []
+    val_losses = []
+    test_losses = []
+    val_f1s = []
+    test_f1s = []
+    final_f1 = 0
+    for cnt in range(5):
+        best_val_loss = 10000
+        best_test_loss = 10000
+        best_train_loss = 10000
+        best_train_f1 = 0
+        best_val_f1 = 0
+        best_test_f1 = 0
+        model = GTN(num_edge=len(A),
+                        num_channels=num_channels,
+                        w_in = node_features.shape[1],
+                        w_out = node_dim,
+                        num_class=num_classes,
+                        num_nodes = node_features.shape[0],
+                        num_layers= num_layers)
+        model.cuda()
+        if adaptive_lr == 'false':
+            optimizer = torch.optim.Adam(model.parameters(), lr=0.005, weight_decay=0.001)
+        else:
+            optimizer = torch.optim.Adam([{'params':model.gcn.parameters()},
+                                        {'params':model.linear1.parameters()},
+                                        {'params':model.linear2.parameters()},
+                                        {"params":model.layers.parameters(), "lr":0.5}
+                                        ], lr=0.005, weight_decay=0.001)
+        loss = nn.CrossEntropyLoss()
+        Ws = []
+        for i in range(30):
+            print('Epoch: ',i+1)
+            for param_group in optimizer.param_groups:
+                if param_group['lr'] > 0.005:
+                    param_group['lr'] = param_group['lr'] * 0.9
+            model.train()
+            model.zero_grad()
+            loss, y_train, _ = model(A, node_features, train_node, train_target)
+            loss.backward()
+            optimizer.step()
+            train_f1 = torch.mean(f1_score(torch.argmax(y_train,dim=1), train_target, num_classes=3)).cpu().numpy()
+            print('Train - Loss: {}, Macro_F1: {}'.format(loss.detach().cpu().numpy(), train_f1))
+            model.eval()
+            # Valid
+            with torch.no_grad():
+                val_loss, y_valid,_ = model.forward(A, node_features, valid_node, valid_target)
+                val_f1 = torch.mean(f1_score(torch.argmax(y_valid,dim=1), valid_target, num_classes=3)).cpu().numpy()
+                print('Valid - Loss: {}, Macro_F1: {}'.format(val_loss.detach().cpu().numpy(), val_f1))
+                test_loss, y_test,W = model.forward(A, node_features, test_node, test_target)
+                test_f1 = torch.mean(f1_score(torch.argmax(y_test,dim=1), test_target, num_classes=3)).cpu().numpy()
+                test_acc = accuracy(torch.argmax(y_test,dim=1), test_target)
+                print('Test - Loss: {}, Macro_F1: {}, Acc: {}\n'.format(test_loss.detach().cpu().numpy(), test_f1, test_acc))
+                if val_f1 > best_val_f1:
+                    best_val_loss = val_loss.detach().cpu().numpy()
+                    best_test_loss = test_loss.detach().cpu().numpy()
+                    best_train_loss = loss.detach().cpu().numpy()
+                    best_train_f1 = train_f1
+                    best_val_f1 = val_f1
+                    best_test_f1 = test_f1
+            torch.cuda.empty_cache()
+        print('---------------Best Results--------------------')
+        print('Train - Loss: {}, Macro_F1: {}'.format(best_test_loss, best_train_f1))
+        print('Valid - Loss: {}, Macro_F1: {}'.format(best_val_loss, best_val_f1))
+        print('Test - Loss: {}, Macro_F1: {}'.format(best_test_loss, best_test_f1))
+
---
+++ model.py
@@ -0,0 +1,137 @@
+import torch
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+from matplotlib import pyplot as plt
+import pdb
+
+
+class GTN(nn.Module):
+    
+    def __init__(self, num_edge, num_channels, w_in, w_out, num_class,num_layers,norm):
+        super(GTN, self).__init__()
+        self.num_edge = num_edge
+        self.num_channels = num_channels
+        self.w_in = w_in
+        self.w_out = w_out
+        self.num_class = num_class
+        self.num_layers = num_layers
+        self.is_norm = norm
+        layers = []
+        for i in range(num_layers):
+            if i == 0:
+                layers.append(GTLayer(num_edge, num_channels, first=True))
+            else:
+                layers.append(GTLayer(num_edge, num_channels, first=False))
+        self.layers = nn.ModuleList(layers)
+        self.weight = nn.Parameter(torch.Tensor(w_in, w_out))
+        self.bias = nn.Parameter(torch.Tensor(w_out))
+        self.loss = nn.CrossEntropyLoss()
+        self.linear1 = nn.Linear(self.w_out*self.num_channels, self.w_out)
+        self.linear2 = nn.Linear(self.w_out, self.num_class)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        nn.init.xavier_uniform_(self.weight)
+        nn.init.zeros_(self.bias)
+
+    def gcn_conv(self,X,H):
+        X = torch.mm(X, self.weight)
+        H = self.norm(H, add=True)
+        return torch.mm(H,X)
+
+    def normalization(self, H):
+        for i in range(self.num_channels):
+            if i==0:
+                H_ = self.norm(H[i,:,:]).unsqueeze(0)
+            else:
+                H_ = torch.cat((H_,self.norm(H[i,:,:]).unsqueeze(0)), dim=0)
+        return H_
+
+    def norm(self, H, add=False):
+        if add == False:
+            H = H*((torch.eye(H.shape[0])==0).type(torch.FloatTensor))
+        else:
+            H = H*((torch.eye(H.shape[0])==0).type(torch.FloatTensor)) + torch.eye(H.shape[0]).type(torch.FloatTensor)
+        deg = torch.sum(H, dim=1)
+        deg_inv = deg.pow(-1)
+        deg_inv[deg_inv == float('inf')] = 0
+        deg_inv = deg_inv*torch.eye(H.shape[0]).type(torch.FloatTensor)
+        H = torch.mm(deg_inv,H)
+        return H
+
+    def forward(self, A, X, target_x, target):
+        A = A.unsqueeze(0).permute(0,3,1,2) 
+        Ws = []
+        for i in range(self.num_layers):
+            if i == 0:
+                H, W = self.layers[i](A)
+            else:
+                H = self.normalization(H)
+                H, W = self.layers[i](A, H)
+            Ws.append(W)
+        
+        #H,W1 = self.layer1(A)
+        #H = self.normalization(H)
+        #H,W2 = self.layer2(A, H)
+        #H = self.normalization(H)
+        #H,W3 = self.layer3(A, H)
+        for i in range(self.num_channels):
+            if i==0:
+                X_ = F.relu(self.gcn_conv(X,H[i]))
+            else:
+                X_tmp = F.relu(self.gcn_conv(X,H[i]))
+                X_ = torch.cat((X_,X_tmp), dim=1)
+        X_ = self.linear1(X_)
+        X_ = F.relu(X_)
+        y = self.linear2(X_[target_x])
+        loss = self.loss(y, target)
+        return loss, y, Ws
+
+class GTLayer(nn.Module):
+    
+    def __init__(self, in_channels, out_channels, first=True):
+        super(GTLayer, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.first = first
+        if self.first == True:
+            self.conv1 = GTConv(in_channels, out_channels)
+            self.conv2 = GTConv(in_channels, out_channels)
+        else:
+            self.conv1 = GTConv(in_channels, out_channels)
+    
+    def forward(self, A, H_=None):
+        if self.first == True:
+            a = self.conv1(A)
+            b = self.conv2(A)
+            H = torch.bmm(a,b)
+            W = [(F.softmax(self.conv1.weight, dim=1)).detach(),(F.softmax(self.conv2.weight, dim=1)).detach()]
+        else:
+            a = self.conv1(A)
+            H = torch.bmm(H_,a)
+            W = [(F.softmax(self.conv1.weight, dim=1)).detach()]
+        return H,W
+
+class GTConv(nn.Module):
+    
+    def __init__(self, in_channels, out_channels):
+        super(GTConv, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.weight = nn.Parameter(torch.Tensor(out_channels,in_channels,1,1))
+        self.bias = None
+        self.scale = nn.Parameter(torch.Tensor([0.1]), requires_grad=False)
+        self.reset_parameters()
+    def reset_parameters(self):
+        n = self.in_channels
+        nn.init.constant_(self.weight, 0.1)
+        if self.bias is not None:
+            fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
+            bound = 1 / math.sqrt(fan_in)
+            nn.init.uniform_(self.bias, -bound, bound)
+
+    def forward(self, A):
+        A = torch.sum(A*F.softmax(self.weight, dim=1), dim=1)
+        return A
